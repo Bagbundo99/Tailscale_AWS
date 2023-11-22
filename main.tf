7644c897b5e9127a9c08e01001c78357c117a434
Bagbundo99 <nacho@test.com>
2023-11-21 21:01:41 -0600

    Added the nsg and the two modules

--- main.tf
+++ main.tf
@@ -19,6 +19,13 @@ provider "azurerm" {
   }
 }
 
+#module info
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.2.0"
+}
+
 #Pull up the latest
 data "aws_ami" "iam" {
   most_recent = true
@@ -79,19 +86,61 @@ module "vpc" {
 
 #Exit node 
 
+module "exit_node" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  name = "exit_node"
+  instance_type = "t2.nano"
+  key_name = aws_key_pair.this.key_name
+  vpc_security_group_ids = []
+  associate_public_ip_address = true
+
+}
+
 
 #Exit node nsg
 #Needs to be open for connection, can be made an script for verifying IP of the user connecting? 
+module "exit_node_nsg" {
+  source = "terraform-aws-modules/security-group/aws"
+  name = "Control_server"
+  description = "Exit server NSG"
+  vpc_id = module.vpc.vpc_id
+   ingress_with_cidr_blocks = [
+    {
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr_blocks = "${module.control_server_ec2.private_ip}/32" 
+    },
+    {
+    from_port = 23
+    to_port = 65355
+    ip_protocol = "tcp"
+    cidr_blocks = "0.0.0.0/0" 
+    }
+    
+  ]
+ 
+}
+
+
 
 #Control server 
+module "control_server_ec2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  name = "control_node"
+  instance_type = "t2.nano"
+  key_name = aws_key_pair.this.key_name
+  vpc_security_group_ids = [module.control_server_sg.vpc_security_group_id]
+  subnet_id = module.vpc.public_subnets
 
+}
 
 #Control server  nsg 
 module "control_server_sg" {
   source = "terraform-aws-modules/security-group/aws"
   name = "Control_server"
   description = "Control server NSG"
-  vpc_id = module.vpc.default_vpc_id
+  vpc_id = module.vpc.vpc_id
   ingress_rules = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
     {
